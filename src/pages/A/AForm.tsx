bc893145dc3360ddeabccab3be649751536371da
maigy <kerwinmai@yeahka.com>
2021-06-01 09:38:13 +0800

    test:post-request

--- src/pages/A/AForm.tsx
+++ src/pages/A/AForm.tsx
@@ -1,101 +1,107 @@
-import { useLocation, useHistory } from 'umi';
-import { useEffect } from 'react';
-import { message } from 'antd';
-import ProForm, { ProFormText, ProFormList, ProFormDigit, ProFormSelect, ProFormGroup } from '@ant-design/pro-form';
+// import { useLocation, useHistory } from 'umi';
+// import { useEffect } from 'react';
+import ProForm, {
+  ProFormText,
+  ProFormList,
+  ProFormDigit,
+  ProFormSelect,
+  ProFormGroup,
+} from '@ant-design/pro-form';
 
-interface Location {
-    query?: object;
-}
-
-const waitTime = (time: number = 100) => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(true);
-        }, time);
-    });
-};
+// interface Location {
+//     query?: object;
+// }
 
 export default () => {
-    useEffect(() => {
-        console.log('useEffect');
-    }, []);
+  // useEffect(() => {
+  //     console.log('useEffect');
+  // }, []);
 
-    const { query } = useLocation() as Location;
-    console.log(query);
+  // const { query } = useLocation() as Location;
+  // console.log(query);
 
-    const history = useHistory();
-    console.log(history);
+  // const history = useHistory();
+  // console.log(history);
 
-    return (
-        <ProForm<{
-            name: string;
-        }>
-            onFinish={async (values: any) => {
-                // await waitTime(500);
-                // console.log(values);
-                // message.success('提交成功');
-                await new Promise((resolve, reject) => {
-                    fetch('http://localhost:8080/a/form?company=xxx&name=mai')
-                        .then(function(response) {
-                            resolve(response.json());
-                        })
-                }).then(res => console.log(res)) 
-            }}
-            params={{}}
-            >
-            <ProFormText
-                id="company"
-                width="md"
-                name="company"
-                label="公司"
-                tooltip="最长为 24 位"
-                placeholder="请输入名称"
+  const testPostApi = () => {
+    new Promise((resolve) => {
+      fetch('http://localhost:8080/a/form', {
+        method: 'POST',
+        body: JSON.stringify({ name: 'mai' }),
+      }).then(function (response) {
+        resolve(response.json());
+      });
+    }).then(() => {});
+  };
+
+  return (
+    <ProForm<{
+      name: string;
+    }>
+      onFinish={async () => {
+        // await waitTime(500);
+        // console.log(values);
+        // message.success('提交成功');
+        await new Promise((resolve) => {
+          fetch('http://localhost:8080/a/form?company=xxx&name=mai').then(function (response) {
+            resolve(response.json());
+          });
+        }).then((res) => console.log(res));
+      }}
+      params={{}}
+    >
+      <ProFormText
+        id="company"
+        width="md"
+        name="company"
+        label="公司"
+        tooltip="最长为 24 位"
+        placeholder="请输入名称"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      />
+      <ProFormList
+        name="users"
+        label="用户信息"
+        initialValue={[
+          {
+            name: '',
+            age: '',
+            sex: '',
+          },
+        ]}
+        creatorButtonProps={false}
+        itemRender={() => {
+          return (
+            <ProFormGroup>
+              <ProFormText
+                id="name"
                 rules={[
-                    {
+                  {
                     required: true,
-                    },
+                  },
                 ]}
-            />
-            <ProFormList
-                name="users"
-                label="用户信息"
-                initialValue={[
-                    {
-                        name: '',
-                        age: '',
-                        sex: '',
-                    },
-                ]}
-                creatorButtonProps={false}
-                itemRender={(action, record) => {
-                    console.log(record)
-                    return (
-                        <ProFormGroup>
-                            <ProFormText
-                                id="name"
-                                rules={[
-                                    {
-                                    required: true,
-                                    },
-                                ]}
-                                name="name"
-                                label="姓名"
-                            />
-                            <ProFormDigit name="age" label="年龄" width="sm" />
-                            <ProFormSelect
-                                label="性别"
-                                name="sex"
-                                width="xs"
-                                valueEnum={{
-                                    man: '男性',
-                                    woman: '女性',
-                                }}
-                            />
-                        </ProFormGroup>
-                    )
+                name="name"
+                label="姓名"
+              />
+              <ProFormDigit name="age" label="年龄" width="sm" />
+              <ProFormSelect
+                label="性别"
+                name="sex"
+                width="xs"
+                valueEnum={{
+                  man: '男性',
+                  woman: '女性',
                 }}
-            >
-                {/* <ProFormText
+              />
+            </ProFormGroup>
+          );
+        }}
+      >
+        {/* <ProFormText
                     rules={[
                         {
                         required: true,
@@ -114,7 +120,12 @@ export default () => {
                         woman: '女性',
                     }}
                 /> */}
-            </ProFormList>
-        </ProForm>
-    )
-}
+      </ProFormList>
+      <div style={{ padding: '0 0 20px' }}>
+        <button id="post-btn" onClick={testPostApi}>
+          POST TEST
+        </button>
+      </div>
+    </ProForm>
+  );
+};
